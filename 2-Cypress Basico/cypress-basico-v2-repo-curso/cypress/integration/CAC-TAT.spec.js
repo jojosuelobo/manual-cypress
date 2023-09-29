32de7b3c2abe92a6beba931c9e58bf9c68b9ba9d
jojosuelobo <josuelobo2000@gmail.com>
2023-09-28 22:36:21 -0300

    Correção das configs

---
+++ 2-Cypress Basico/cypress-basico-v2-repo-curso/cypress/integration/CAC-TAT.spec.js
@@ -0,0 +1,8 @@
+/// <reference types="Cypress" />
+
+describe('Central de Atendimento ao Cliente TAT', function() {
+    it('verifica o título da aplicação', function() {
+  
+    })
+  })
+  
--- 2-Cypress Basico/cypress/integration/CAC-TAT.spec.js
+++
@@ -1,6 +0,0 @@
-// CAC-TAT.spec.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
