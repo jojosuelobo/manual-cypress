38e698924bed8478ca1e6123dc8ea83aea90dc31
jojosuelobo <josuelobo2000@gmail.com>
2023-09-28 22:57:50 -0300

    [7]: Verifica title

--- 2-Cypress Basico/cypress-basico-v2-repo-curso/cypress/integration/CAC-TAT.spec.js
+++ 2-Cypress Basico/cypress-basico-v2-repo-curso/cypress/integration/CAC-TAT.spec.js
@@ -2,7 +2,8 @@
 
 describe('Central de Atendimento ao Cliente TAT', function() {
     it('verifica o título da aplicação', function() {
-  
+      cy.visit('../../src/index.html');
+      cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
   })
   
