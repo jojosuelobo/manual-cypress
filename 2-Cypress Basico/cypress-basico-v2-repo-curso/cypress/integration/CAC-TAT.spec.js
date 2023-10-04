e52ca72a895a78d62dc5182a549939e8f61d8b53
jojosuelobo <josuelobo2000@gmail.com>
2023-10-04 15:57:00 -0300

    [17]

--- 2-Cypress Basico/cypress-basico-v2-repo-curso/cypress/integration/CAC-TAT.spec.js
+++ 2-Cypress Basico/cypress-basico-v2-repo-curso/cypress/integration/CAC-TAT.spec.js
@@ -1,9 +1,56 @@
 /// <reference types="Cypress" />
 
-describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
-      cy.visit('../../src/index.html');
-      cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
-    })
+describe('Central de Atendimento ao Cliente TAT', function () {
+  beforeEach(() => {
+    cy.visit('../../src/index.html');
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
   })
-  
+
+  it('Preenche campos obrigatórios e envia formulário', function () {
+    const lorem = 'LONG TEXT lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem '
+    cy.get('[id="firstName"]').as('nome').type('Josue')
+    cy.get('[id="lastName"]').as('sobrenome').type('Lobo')
+    cy.get('[id="email"]').as('email').type('josuelobo@email.com')
+    cy.get('[id="open-text-area"]').as('text').type(lorem, { delay: 0 })
+    cy.get('[type="submit"]').click()
+
+    cy.get('.success').should('be.visible')
+    cy.contains('strong', 'Mensagem enviada com sucesso.')
+  })
+
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function () {
+    cy.get('[id="email"]')
+      .as('email')
+      .type('josuelobo')
+
+    cy.contains('button', 'Enviar').click()
+    cy.get('.error').should('be.visible')
+  })
+
+  it('Valida campo de telefone', () => {
+    cy.get('#phone')
+      .type('abcdefg')
+      .should('have.value', '')
+  });
+
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+    cy.get('[id="firstName"]').type('Josue')
+    cy.get('[id="lastName"]').type('Lobo')
+    cy.get('[id="email"]').type('josuelobo@email.com')
+    cy.get('#phone-checkbox').click()
+    cy.get('[id="open-text-area"]').as('text').type('lorem')
+    cy.get('[type="submit"]').click()
+    cy.get('.error').should('be.visible')
+  });
+
+  it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
+    cy.reload()
+    cy.get('[type="submit"]').click()
+    cy.get('.error').should('be.visible')
+  });
+
+  it('envia o formuário com sucesso usando um comando customizado', () => {
+    cy.fillMandatoryFieldsAndSubmit()
+    cy.get('.success').should('be.visible')
+  });
+})
--- 2-Cypress Basico/cypress-basico-v2-repo-curso/cypress/support/commands.js
+++ 2-Cypress Basico/cypress-basico-v2-repo-curso/cypress/support/commands.js
@@ -1,25 +1,7 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', function() {
+    cy.get('[id="firstName"]').as('nome').type('Josue')
+    cy.get('[id="lastName"]').as('sobrenome').type('Lobo')
+    cy.get('[id="email"]').as('email').type('josuelobo@email.com')
+    cy.get('[id="open-text-area"]').as('text').type('lorem')
+    cy.get('[type="submit"]').click()
+})
