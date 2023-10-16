ea0ff23c295f8444f04aec110fec9e28c472ef95
jojosuelobo <josue.lobo@etaure.com.br>
2023-10-16 11:06:42 -0300

    [18]

--- 2-Cypress Basico/cypress-basico-v2-repo-curso/cypress/integration/CAC-TAT.spec.js
+++ 2-Cypress Basico/cypress-basico-v2-repo-curso/cypress/integration/CAC-TAT.spec.js
@@ -53,4 +53,8 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     cy.fillMandatoryFieldsAndSubmit()
     cy.get('.success').should('be.visible')
   });
+
+  it.only('seleciona um produto (YouTube) por seu texto', () => {
+    cy.get('#product').select('YouTube')
+  });
 })
