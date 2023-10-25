b89c47a1c08d78dde6a0a695ae2878d2506ec6da
josue <josue@octuspay.com>
2023-10-25 11:08:34 -0300

    [31]

--- 2-Cypress Basico/cypress-basico-v2-repo-curso/cypress/integration/CAC-TAT.spec.js
+++ 2-Cypress Basico/cypress-basico-v2-repo-curso/cypress/integration/CAC-TAT.spec.js
@@ -76,7 +76,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     cy.get('input[type="radio"][value="feedback"]').check().should('be.checked')
   });
 
-  it.only('marca cada tipo de atendimento', () => {
+  it('marca cada tipo de atendimento', () => {
     cy.get('input[type="radio"]')
       .should('have.length', 3)
       .each(($radio) => {
@@ -84,4 +84,37 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.wrap($radio).should('be.checked')
       })
   });
+
+  it('seleciona um arquivo da pasta fixtures', () => {
+    cy.get('input[type="file"]#file-upload')
+      .should('not.have.value')
+      .selectFile('cypress/fixtures/example.json')
+      .should(($input) => {
+        console.log($input)
+        expect($input[0].files[0].name).to.equal('example.json')
+      })
+  });
+
+  it('seleciona um arquivo simulando um drag-and-drop', () => {
+    cy.get('input[type="file"]#file-upload')
+      .should('not.have.value')
+      .selectFile('cypress/fixtures/example.json', { action: 'drag-drop' })
+      .should(($input) => {
+        console.log($input)
+        expect($input[0].files[0].name).to.equal('example.json')
+      })
+  });
+
+  it('eleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
+    cy.fixture('example.json').as('sampleFile')
+    cy.get('input[type="file"]#file-upload')
+      .should('not.have.value')
+      .selectFile('@sampleFile')
+      .should(($input) => {
+        console.log($input)
+        expect($input[0].files[0].name).to.equal('example.json')
+      })
+  });
+
+
 })
