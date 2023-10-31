55d14bd23c8dc60ebaac66608f4d57699e24c472
josue <josue@octuspay.com>
2023-10-31 11:23:09 -0300

    BoasPraticas: [18]

--- 5-Boas Praticas/boas-praticas-em-automacao-de-testes-com-cypress-v2/cypress/e2e/hardcoded-assertion/sample.cy.js
+++ 5-Boas Praticas/boas-praticas-em-automacao-de-testes-com-cypress-v2/cypress/e2e/hardcoded-assertion/sample.cy.js
@@ -1,3 +1,5 @@
+const { hits } = require('../../fixtures/stories')
+
 describe('Hardcoded assertion bad practice', () => {
   beforeEach(() => {
     cy.intercept(
@@ -11,17 +13,50 @@ describe('Hardcoded assertion bad practice', () => {
   })
 
   it('searches', () => {
+    cy.fixture('stories')
+      .then(({ hits }) => {
+        cy.search('cypress.io')
+        cy.wait('@getStories')
+
+        cy.get('.table-row')
+          .as('tableRows')
+          .should('have.length', hits.length)
+
+        hits.forEach((hit, index) => {
+          cy.get('@tableRows')
+            .eq(index)
+            .should('contain', hit.title)
+        });
+      })
+
+    // cy.search('cypress.io')
+    // cy.wait('@getStories')
+
+    // cy.get('.table-row')
+    //   .as('tableRows') 
+    //   .should('have.length', 2)
+    // cy.get('@tableRows')
+    //   .eq(0)
+    //   .should('contain', 'Agile Testing')
+    // cy.get('@tableRows')
+    //   .eq(1)
+    //   .should('contain', 'Clean Code')
+  })
+
+  // Versão OTIMIZADA
+  it('searches - desestruturação de objetos do JavaScript', () => {
     cy.search('cypress.io')
     cy.wait('@getStories')
 
     cy.get('.table-row')
       .as('tableRows')
-      .should('have.length', 2)
-    cy.get('@tableRows')
-      .eq(0)
-      .should('contain', 'Agile Testing')
-    cy.get('@tableRows')
-      .eq(1)
-      .should('contain', 'Clean Code')
+      .should('have.length', hits.length)
+
+    hits.forEach((hit, index) => {
+      cy.get('@tableRows')
+        .eq(index)
+        .should('contain', hit.title)
+    });
   })
+
 })
